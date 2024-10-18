6bf1e3ab72830250711824dd84507efb46dad910
刘煌 <225225@nd.com>
2024-10-18 17:15:50 +0800

    修改 CMakeLists.txt 解决编译双MCU架构警告，使用-mcpu=cortex-m7，移除-march=armv7e-m

---
+++ CMakeFiles/xlat.bin.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/xlat.bin"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/xlat.bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
